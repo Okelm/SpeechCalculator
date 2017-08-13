be51f71b12a33b33b655e78a54a1b72616654bd8
okelm <bartlomiejwidlarz@gmail.com>
2017-08-13 19:55:11 +0200

    Adding layout with databinding

--- app/src/main/java/com/bwidlarz/speechcalculator/MainActivity.kt
+++ app/src/main/java/com/bwidlarz/speechcalculator/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.bwidlarz.speechcalculator
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bwidlarz.speechcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
