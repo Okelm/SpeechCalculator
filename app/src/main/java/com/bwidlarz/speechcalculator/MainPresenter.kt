0ba78dc036124cd87fb687535f861cefd11003c9
okelm <bartlomiejwidlarz@gmail.com>
2017-08-15 18:32:16 +0200

    Adding onContinueClicked functionality

--- app/src/main/java/com/bwidlarz/speechcalculator/MainPresenter.kt
+++ app/src/main/java/com/bwidlarz/speechcalculator/MainPresenter.kt
@@ -1,5 +1,6 @@
 package com.bwidlarz.speechcalculator
 
+import android.util.Log
 import com.bwidlarz.speechcalculator.common.BasePresenter
 import com.bwidlarz.speechcalculator.common.evaluate
 import com.bwidlarz.speechcalculator.common.isNumberOrSymbol
@@ -8,12 +9,34 @@ import java.util.*
 class MainPresenter : BasePresenter<SpeechView>() {
 
     fun loadSpeech(results: ArrayList<String>) {
+
+        for (result in results){
+            Log.d("asdf", result)
+        }
         val result = results
                 .filter { isNumberOrSymbol(it) }
                 .asReversed()
                 .firstOrNull()
 
-       withView { if (!result.isNullOrEmpty()) onRecognitionFinished(result!!) else onRecognitionError("Please try again!")}
+       withView {
+           if (!result.isNullOrEmpty()) onRecognitionFinished(result!!)
+           else onRecognitionError("Please try again!")
+       }
+    }
+
+    fun loadSpeechWithPrevious(results: ArrayList<String>, previousResult: String) {
+        for (result in results){
+            Log.d("asdf", result)
+        }
+        val result = results
+                .filter { isNumberOrSymbol(it) }
+                .asReversed()
+                .firstOrNull()
+
+        withView {
+            if (!result.isNullOrEmpty()) onRecognitionFinished(previousResult + " " + result!!)
+            else onRecognitionError("Please try again!")
+        }
     }
 
     fun evaluateExpression(stringExpression: String) {
